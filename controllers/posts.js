9047639a0eae86781619623155e82a04b0376eac
Jacob Flora <jacobflora4@gmail.com>
2021-03-16 23:55:46 -0400

    routing for like btn complete

--- controllers/posts.js
+++ controllers/posts.js
@@ -29,19 +29,28 @@ export const updatePost = async (req, res) => {
 
   if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No posts with that id.');
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
   res.json(updatedPost);
 }
 
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
 
-// This is the POST template.
-// export const createPost = (req, res) => {
-//   const body = req.body;
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No posts with that id.');
 
-//   try {
+  await PostMessage.findByIdAndRemove(id);
 
-//   } catch (error) {
+  res.json({ message: 'Post deleted successfully.' });
+}
 
-//   }
-// }
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No posts with that id.');
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+  res.json(updatePost)
+}
